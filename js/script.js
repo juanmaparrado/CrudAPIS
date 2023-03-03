74b198c563bafd5d55be087cb139126e09be7f5f
juanmaparrado <juanmanuel.parrado.fp@iescampanillas.com>
2023-03-03 11:31:59 +0100

    add mostrar

--- js/script.js
+++ js/script.js
@@ -10,6 +10,16 @@ const requests = countries.map(country => fetch(`https://world-population3.p.rap
 let datos = [];
 let data = null;
 
+//LLAMADA AJAX
+function llamadaAjax(url, datos, metodo) {
+  return $.ajax({
+      url: url, 
+      data: datos, 
+      type: metodo,
+      dataType: "json"
+  });
+}
+
 document.getElementById('generate-btn').addEventListener('click', () => {
   Promise.all(requests)
     .then(responses => Promise.all(responses.map(response => response.json())))
@@ -22,15 +32,6 @@ document.getElementById('generate-btn').addEventListener('click', () => {
     .catch(err => console.error(err));
 });
 
-//LLAMADA AJAX
-function llamadaAjax(url, datos, metodo) {
-  return $.ajax({
-      url: url, 
-      data: datos, 
-      type: metodo,
-      dataType: "json"
-  });
-}
 document.getElementById('save-btn').addEventListener('click', () => {
   let peticionAjax = llamadaAjax('./php/guardar.php', JSON.stringify(datos), 'POST'); 
   console.log("----------DATOS GRABADOS---------");
@@ -40,4 +41,51 @@ document.getElementById('save-btn').addEventListener('click', () => {
 document.getElementById('delete-btn').addEventListener("click", () => {
   let peticionAjax = llamadaAjax('./php/borrar.php', JSON.stringify(datos), 'POST');
   console.log("---------DATOS ELIMINADOS---------");
+});
+
+document.getElementById('show-btn').addEventListener("click", () => {
+  llamadaAjax('./php/mostrar.php', null, 'GET')
+    .then(response => {
+      console.log(response);
+      console.log("MOSTRANDO DATOS")
+      var outputDiv = document.getElementById("output");
+      outputDiv.innerHTML="";
+      // Crear una tabla HTML
+      var table = document.createElement("table");
+      
+      // Agregar las etiquetas thead y tbody a la tabla
+      var tableHead = table.createTHead();
+      var tableBody = table.createTBody();
+      
+      // Crear las filas de encabezado de la tabla
+      var headerRow = tableHead.insertRow();
+      var headerCell1 = document.createElement("th");
+      headerCell1.textContent = "Country";
+      headerRow.appendChild(headerCell1);
+      var headerCell2 = document.createElement("th");
+      headerCell2.textContent = "Population1970";
+      headerRow.appendChild(headerCell2);
+      var headerCell3 = document.createElement("th");
+      headerCell3.textContent = "Population2022";
+      headerRow.appendChild(headerCell3);
+      var headerCell4 = document.createElement("th");
+      headerCell4.textContent = "Area";
+      headerRow.appendChild(headerCell4);
+      
+      // Recorrer los datos del objeto JSON y agregar las filas de datos
+      for (var i = 0; i < response.length; i++) {
+        var dataRow = tableBody.insertRow();
+        var dataCell1 = dataRow.insertCell();
+        dataCell1.textContent = response[i].country;
+        var dataCell2 = dataRow.insertCell();
+        dataCell2.textContent = response[i].population1970;
+        var dataCell3 = dataRow.insertCell();
+        dataCell3.textContent = response[i].population2022;
+        var dataCell4 = dataRow.insertCell();
+        dataCell4.textContent = response[i].area;
+      }
+
+      outputDiv.appendChild(table);
+    })
+    .catch(error => console.error(error));
 });
---
+++ php/mostrar.php
@@ -0,0 +1,20 @@
+<?php
+define("HOSTNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "root");
+define("DATABASE", "valores");
+
+$conexion = new mysqli(HOSTNAME,USERNAME,PASSWORD,DATABASE);
+if ($conexion->connect_error) {
+    die("Error al conectar a la base de datos: " . $conn->connect_error);
+  }
+
+$query ="SELECT  `country`, `population1970`, `population2022`, `area` FROM `tabla_datos`";
+$result=mysqli_query($conexion,$query) or die(mysqli_error($conexion));
+
+$datos = [];
+while ($row = mysqli_fetch_array($result)) {
+    array_push($datos, $row);
+}
+echo json_encode($datos);
+mysqli_close($conexion);
