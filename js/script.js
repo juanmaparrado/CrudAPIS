a04c7785ece5311ff9eaef661328b45fd5b4c504
juanmaparrado <juanmanuel.parrado.fp@iescampanillas.com>
2023-03-03 10:24:30 +0100

    add countries

--- js/script.js
+++ js/script.js
@@ -5,28 +5,21 @@ const options = {
     'X-RapidAPI-Host': 'world-population3.p.rapidapi.com'
   }
 };
-const countries = ['FRA', 'ITA', 'ESP','POL','UKR'];
+const countries = ['FRA', 'ITA', 'ESP','POL','UKR','ALB','AND', 'AUT','BLR','BEL','HRV','CZE','DEU', 'NLD', 'CHE', 'SWE', 'NOR', 'FIN', 'DNK', 'ISL','DZA','AGO','CMR','CAF','COD','GNQ','GHA','GIN','CIV','KEN','MDG','AUS','PNG','NZL','FJI','FJI','VAT','GBR','ROU','ZWE'];
 const requests = countries.map(country => fetch(`https://world-population3.p.rapidapi.com/${country}`, options));
 let datos = [];
 let data = null;
 
-function generarDatos() {
+document.getElementById('generate-btn').addEventListener('click', () => {
   Promise.all(requests)
     .then(responses => Promise.all(responses.map(response => response.json())))
     .then(data => {
-      datos=[];
       data.forEach(({ Country, '1970 Population':Population1970, '2022 Population': Population2022, 'Area (km²)':Area }) => {
         datos.push({ country: Country, population1970: Population1970, population2022: Population2022, area: Area });
       });
       console.log("GENERANDO LOS DATOS DESDE LA API");
-      console.log(datos);
   })
     .catch(err => console.error(err));
-}
-
-document.getElementById('generate-btn').addEventListener('click', () => {
-  datos=[];
-  generarDatos();
 });
 
 //LLAMADA AJAX
@@ -40,12 +33,11 @@ function llamadaAjax(url, datos, metodo) {
 }
 document.getElementById('save-btn').addEventListener('click', () => {
   let peticionAjax = llamadaAjax('./php/guardar.php', JSON.stringify(datos), 'POST'); 
-  console.log("DATOS GRABADOS");
-  console.log("--------------");
+  console.log("----------DATOS GRABADOS---------");
+
 });
 
 document.getElementById('delete-btn').addEventListener("click", () => {
   let peticionAjax = llamadaAjax('./php/borrar.php', JSON.stringify(datos), 'POST');
-  console.log("DATOS ELIMINADOS");
-  console.log("----------------");
+  console.log("---------DATOS ELIMINADOS---------");
 });
